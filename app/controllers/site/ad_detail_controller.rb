f07033972275c34b4514904add95c36f82830954
Laerte <laerte.mneto@gmail.com>
2017-10-25 15:46:02 -0300

    adicionando json para testes em paginas

--- app/controllers/site/ad_detail_controller.rb
+++ app/controllers/site/ad_detail_controller.rb
@@ -4,6 +4,12 @@ class Site::AdDetailController < SiteController
     def show
     	@ad = Ad.find(params[:id])
         @categories = Category.all.order_by_description
+
+        respond_to do |format|
+            format.html #views htaml.erb
+            format.json { render json: @ad, except: [:description, :description_md]}
+            format.xml { render xml: @ad }
+        end
     end
 
 end
